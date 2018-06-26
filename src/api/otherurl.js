da064fadaba3657d0ab0765f2e6d65f7340d943b
BoBo <2274580253@qq.com>
2018-06-26 21:16:43 +0800

    Update otherurl.js

--- src/api/otherurl.js
+++ src/api/otherurl.js
@@ -1,6 +1,6 @@
 const outUrl = {
-    payurl:"http://news.isxuan.com",   
-    infourl:"http://lchyhfx.52qingqiyun.com"   //手机端详情   https://hyhwap.52qingqiyun.com      http://lchyhfx.52qingqiyun.com
+    payurl:"",   
+    infourl:""   
 
 }
 export default outUrl;
